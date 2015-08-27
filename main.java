171147c41fc4ea1c19a0af7b4ea9724648bd35d6
punkkk <punkrus18@gmail.com>
2015-08-27 17:36:37 +0300

    some improves

--- main.java
+++ main.java
@@ -5,20 +5,29 @@ import java.io.IOException;
 //MY GREP)))0
 class main{
 	public static void main(String[] args) throws IOException, FileNotFoundException{
-		BufferedReader reader = new BufferedReader(new FileReader("text.txt"));
-
-		String line, buf;
-		Boolean isnothing = true;
-		while ( ( line = reader.readLine() ) != null){
-			buf = line;
-			buf.toLowerCase();
-			if( buf.indexOf(args[0].toLowerCase()) != -1 ){
-				System.out.println(line);
-				isnothing = false;
+		String path;
+		if( args.length < 1){
+			System.out.println("GOOD BYE MISTER!!!!1!");
+		}else{
+			if( args.length == 2 ){
+				path = args[1];
+			}else{
+				path = "text.txt";
 			}
-		}
-		if(isnothing){
-			System.out.println("Your string has not found here");
+			BufferedReader reader = new BufferedReader(new FileReader(path));
+			String line, buf;
+			Boolean isnothing = true;
+			while ( ( line = reader.readLine() ) != null){
+				buf = line;
+				buf.toLowerCase();
+				if( buf.indexOf(args[0].toLowerCase()) != -1 ){
+					System.out.println(line);
+					isnothing = false;
+				}
+			}
+			if(isnothing){
+				System.out.println("I can't found your string here((");
+			}
 		}
 	}
 }
